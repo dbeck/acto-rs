8261703189e1fcb2174b1dd8f2a5f4b18dd816c4
David Beck <david.beck.priv@gmail.com>
2016-07-25 22:18:15 +0100

    removing leftover files

--- src/identified_receiver.rs
+++
@@ -1,9 +0,0 @@
-extern crate lossyq;
-use self::lossyq::spsc::Receiver;
-use super::channel_id;
-use super::common::Message;
-
-pub struct IdentifiedReceiver<Input: Copy+Send> {
-  pub id    : channel_id::Id,
-  pub input : Receiver<Message<Input>>,
-}
