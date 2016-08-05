6aeac59c929e297f82f68119846cfb12a56fadd9
David Beck <david.beck.priv@gmail.com>
2016-08-06 00:23:12 +0100

    make collector more reliable

--- src/scheduler/collector.rs
+++ src/scheduler/collector.rs
@@ -20,42 +20,57 @@ impl Gather for Collector {
           input_vec:   Vec<&mut Receiver<Message<Self::InputType>>>,
           output:      &mut Sender<Message<Self::OutputType>>) -> Schedule {
 
-    // this closure sends a value to the output channel, but also checks if
-    // we were replacing an item in the output queue. if so it saves that in
-    // the overflow queue and returns false. otherwise true.
-    let mut send_value = |self_val: &mut Self, mut val: Option<Message<Self::InputType>>| {
-      // put value in the output queue
-      output.put( |o| { mem::swap(&mut val, o); });
+    {
+      // this closure sends a value to the output channel, but also checks if
+      // we were replacing an item in the output queue. if so it saves that in
+      // the overflow queue and returns false. otherwise true.
+      let mut send_value = |self_val: &mut Self, mut val: Option<Message<Self::InputType>>| {
+        // put value in the output queue
+        output.put( |o| { mem::swap(&mut val, o); });
 
-      // check the result of put()
-      match val {
-        Some(Message::Value(task)) => {
-          // saving the replaced item
-          self_val.overflow.push_back(task);
-          false
-        },
-        _ => { true }
+        // check the result of put()
+        match val {
+          Some(Message::Value(task)) => {
+            // saving the replaced item
+            self_val.overflow.push_back(task);
+            false
+          },
+          _ => { true }
+        }
+      };
+
+      // process the previously overflown items
+      loop {
+        match self.overflow.pop_front() {
+          Some(item) => {
+            let optional_item : Option<Message<Self::InputType>> = Some(Message::Value(item));
+            if send_value(self, optional_item) == false {
+              break;
+            }
+          },
+          None => { break; }
+        }
       }
-    };
 
-    // process the previously overflown items
-    loop {
-      match self.overflow.pop_front() {
-        Some(item) => {
-          let optional_item : Option<Message<Self::InputType>> = Some(Message::Value(item));
-          if send_value(self, optional_item) == false {
-            break;
-          }
-        },
-        None => { break; }
+      // process the incoming items
+      for input in input_vec {
+        for item in input.iter() {
+          let optional_item : Option<Message<Self::InputType>> = Some(item);
+          send_value(self, optional_item);
+        }
       }
     }
 
-    // process the incoming items
-    for input in input_vec {
-      for item in input.iter() {
-        let optional_item : Option<Message<Self::InputType>> = Some(item);
-        send_value(self, optional_item);
+    // check tmp value of output channel
+    {
+      let mut val : Option<Message<Self::InputType>> = None;
+      output.tmp( |t| { mem::swap(&mut val, t); });
+      match val {
+        Some(Message::Value(task)) => {
+          // saving the replaced item
+          self.overflow.push_back(task);
+        },
+        _ => {}
       }
     }
 
